696be24845b79abe080dfb594da353e45bf10bd2
mrcharles07 <samir07ali@gmail.com>
2020-12-13 20:29:35 +0530

    First C++ program

---
+++ 2_Data_Types.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int a;
+    cout <<"size of int(a):"<< sizeof(a)<<" byte"<<endl;
+
+    char b;
+    cout <<"size of char(b):"<< sizeof(b)<<" byte"<<endl;
+
+    float c;
+    cout <<"size of float(c):"<< sizeof(c)<<" byte"<<endl;
+
+    double d;
+    cout <<"size of double(d):"<< sizeof(d)<<" byte"<<endl;
+
+    short e;
+    cout <<"size of short(e):"<< sizeof(e)<<" byte"<<endl;
+
+    return 0;
+}
