24ab9ae437ddd23bccd2453509a77643224677e9
mrcharles07 <samir07ali@gmail.com>
2020-12-14 22:18:09 +0530

    Basic I/O Operation

---
+++ 3_Input-Output.cpp
@@ -0,0 +1,37 @@
+/* output opertaion
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    char ary[]="Welcome to C++ programming";
+    cout <<"Value of ary is:"<< ary<<endl;
+    cout <<"Learn C++ Language\n";
+    cout <<"Learn Java Language\n";
+}
+*/
+
+// Input operation
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int age;
+    cout<<"Enter your Age:"<<endl;
+    cin>>age;
+    cout<<"your age is:"<<age<<endl;
+}
+
+
+/* End line operation
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    cout<<"Learn C Programming"<<endl;
+    cout<<"Learn C++ Programming"<<endl;
+    cout<<"Learn Java Programming"<<endl;
+}
+*/
