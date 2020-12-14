444607817d5e20c7d52784880e4416f8e4a33613
mrcharles07 <samir07ali@gmail.com>
2020-12-14 21:06:29 +0530

    Add some statement

--- 2_Data_Types.cpp
+++ 2_Data_Types.cpp
@@ -6,17 +6,56 @@ int main()
     int a;
     cout <<"size of int(a):"<< sizeof(a)<<" byte"<<endl;
 
-    char b;
-    cout <<"size of char(b):"<< sizeof(b)<<" byte"<<endl;
+    signed int b;
+    cout <<"size of signed int(b):"<< sizeof(b)<<" byte"<<endl;
 
-    float c;
-    cout <<"size of float(c):"<< sizeof(c)<<" byte"<<endl;
+    unsigned int c;
+    cout <<"size of unsigned int(c):"<< sizeof(c)<< "byte"<<endl;
 
-    double d;
-    cout <<"size of double(d):"<< sizeof(d)<<" byte"<<endl;
+    char d ;
+    cout <<"size of char(d):"<< sizeof(d)<<" byte"<<endl;
 
-    short e;
-    cout <<"size of short(e):"<< sizeof(e)<<" byte"<<endl;
+    signed char e;
+    cout <<"size of signed char(e):"<< sizeof(e)<<" byte"<<endl;
+
+    unsigned char f;
+    cout <<"size of unsigned char(f):"<< sizeof(f)<<" byte"<<endl;
+
+    short g;
+    cout <<"size of short(g):"<< sizeof(g)<<" byte"<<endl;
+
+    signed short h;
+    cout <<"size of signed short(h):"<< sizeof(h)<< "byte"<<endl;
+
+    unsigned short i;
+    cout <<"size of unsigned short(i):"<< sizeof(i)<< "byte"<<endl;
+    
+    short int j;
+    cout <<"size of short int(j):"<< sizeof(j)<< "byte"<<endl;
+
+    signed short int k;
+    cout <<"size of short int(k):"<< sizeof(k)<< "byte"<<endl;
+
+    unsigned short int l;
+    cout <<"size of unsigned short int(l):"<< sizeof(l)<< "byte"<<endl;
+
+    long int m;
+    cout <<"size of long int(m):"<< sizeof(m)<<" byte"<<endl;
+
+    signed long int n;
+    cout <<"size of signed long int(n):"<< sizeof(n)<<" byte"<<endl;
+
+    unsigned long int o;
+    cout <<"size of unsigned long int(o):"<< sizeof(o)<<" byte"<<endl;
+
+    float p;
+    cout <<"size of float(p):"<< sizeof(p)<<" byte"<<endl;
+
+    double q;
+    cout <<"size of double(q):"<< sizeof(q)<<" byte"<<endl;
+
+    long double r;
+    cout <<"size of long double(r):"<< sizeof(r)<< "byte"<<endl;
 
     return 0;
 }
