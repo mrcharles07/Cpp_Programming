ce0729cf6f92f063fb1e196fbe2ad38fc2cd9a1e
mrcharles07 <samir07ali@gmail.com>
2020-12-15 21:06:16 +0530

    Comments in C++

---
+++ 5_Comments.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    //Single line comment
+
+    cout <<"Here is a single line comment"<<endl;
+
+    /*Multi line comment
+
+    cout <<"Here is a multi line comment"<<endl;
+
+    */
+}
