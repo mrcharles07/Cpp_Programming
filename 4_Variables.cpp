e10176043610aa6fb99a5b1802b90a0a077af829
mrcharles07 <samir07ali@gmail.com>
2020-12-15 19:54:46 +0530

    Variables example

---
+++ 4_Variables.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main()
+
+{
+    //Initiazing a variables
+    int a=5; //Declaration of variables
+    int age; //Declaration of multiple variables
+
+    cout <<"The value of a is:"<<a<<endl;
+    cout <<"What is your Age:"<<endl;
+    cin>>age;
+    cout <<"your age is :"<<age<<endl;
+    return 0;
+}
