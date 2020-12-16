c53677a24032ab523d73fdb3930f78ae82309ec1
mrcharles07 <samir07ali@gmail.com>
2020-12-16 23:55:18 +0530

    Some extra operators

--- 6_Operators.cpp
+++ 6_Operators.cpp
@@ -5,7 +5,7 @@ int main()
 
 {   
     
-    int a=2, b=3;
+    int a=2, b=3, m, n, o;
     int x=1, y=2, z=3;
     
     //Unary operator of Post Increment(a++)& Pre(++a) Increment
@@ -20,12 +20,33 @@ int main()
     cout <<z%y<<endl; //Modulas
 
     //Relational Operator (<, >, <=, >=, ==, !=)
-    if (a>b)
-        cout <<"a is greater than b"<<endl;
+    cout <<"Enter the value of m"<<endl;
+    cin >>m;
+    if (a>m)
+        cout <<"a is greater than m"<<endl;
+    else if (x<m)
+        cout <<"x is smaller than m"<<endl;
     else
     {
-        cout <<"a is smaller than b"<<endl;
+        cout <<"a is equal to m"<<endl;
     }
+
+    //Logical Operator
+    cout <<"Enter the value of n"<<endl;
+    cin >>n;
+    cout <<"Enter the value of o"<<endl; 
+    cin >>o;
+    if (n==5 && o==5)
+        cout <<"n and o are equal"<<endl;
+    else
+    {
+        cout <<"n and o are not equal"<<endl;
+    }
+
+    //
+    
+
+
     
 
     return 0;
